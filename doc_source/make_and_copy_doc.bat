92c2908d1bf7feb75204bd4ec3bb9b5cf5d0cccc
Pierre-Yves Yanni <pyanni@gmail.com>
2020-10-28 18:25:30 -0400

    will the bilingual doc work now?

--- doc_source/make_and_copy_doc.bat
+++ doc_source/make_and_copy_doc.bat
@@ -1,5 +1,5 @@
-call sphinx-build -b latex ./source build/latex/en -D language='en'
-call sphinx-build -b latex ./source build/latex/fr -D language='fr'
+call sphinx-build -b latex ./source build/latex/en
+call sphinx-build -b latex ./source build/latex/fr
 call sphinx-build -b html ./source build/html/en -D language='en' 
 call sphinx-build -b html ./source build/html/fr -D language='fr'
 
