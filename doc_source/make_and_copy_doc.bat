18711e8d15a3d77346c40f3879f78909fff339e3
Pierre-Yves Yanni <pyanni@gmail.com>
2020-10-28 18:41:34 -0400

    i hate .bat files - or why this doc cant work

--- doc_source/make_and_copy_doc.bat
+++ doc_source/make_and_copy_doc.bat
@@ -1,6 +1,1 @@
-call sphinx-build -b latex ./source build/latex/en
-call sphinx-build -b latex ./source build/latex/fr
-call sphinx-build -b html ./source build/html/en -D language='en' 
-call sphinx-build -b html ./source build/html/fr -D language='fr'
-
 xcopy "build/html" /s "../docs" /y
