f2394535b4de3f6fbc43d760554cdb1a336ab8ff
Pierre-Yves Yanni <pyanni@gmail.com>
2020-10-28 18:06:41 -0400

    bilingual doc does not work

--- doc_source/make_and_copy_doc.bat
+++ doc_source/make_and_copy_doc.bat
@@ -1,5 +1,6 @@
-del /q /s build
-del /q /s "source/_build"
-call "make.bat" latexpdf
-call "make.bat" html
+call sphinx-build -b latex ./source build/latex/en -D language='en'
+call sphinx-build -b latex ./source build/latex/fr -D language='fr'
+call sphinx-build -b html ./source build/html/en -D language='en' 
+call sphinx-build -b html ./source build/html/fr -D language='fr'
+
 xcopy "build/html" /s "../docs" /y
