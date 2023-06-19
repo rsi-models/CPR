665615109fb08ba5171ddb15042641b21a04fd8f
loriggio <64011040+loriggio@users.noreply.github.com>
2023-06-19 17:23:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setuptools.setup(
-    name="cprrsi",
+    name="cpr-rsi",
     version="1.2.2",
     description="Assessing Canadians' Preparation for Retirement",
     long_description=README,
